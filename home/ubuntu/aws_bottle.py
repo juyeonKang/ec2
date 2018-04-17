d71270f6ce8dfadece2eb8baf8dc659bb2fb3070
JY <lovedct_jy@naver.com>
2018-04-17 12:03:27 +0000

    add notice_You should select at least one column

--- home/ubuntu/aws_bottle.py
+++ home/ubuntu/aws_bottle.py
@@ -19,27 +19,36 @@ def db():
 	str2 = " FROM books JOIN Rtable ON ISBN = b_ISBN JOIN authors ON AuID = a_AuID JOIN publishers ON b_PubID = p_PubID"
 	str3 = " ORDER BY "
 	
-	input_str = request.POST.getall('column')
-	column = ['< '+x[2:]+' >' for x in input_str]
-	column = [tuple(column)]
-	input_str = [x+' text,' for x in input_str]
-	input_str = ''.join(input_str)[:-1]
+	if request.POST.column:	
+		input_str = request.POST.getall('column')
+		column = ['< '+x[2:]+' >' for x in input_str]
+		column = [tuple(column)]
+		input_str = [x+' text,' for x in input_str]
+		input_str = ''.join(input_str)[:-1]
 
-	#### will add 'order'(list box) in '8080.html' to sorting 
-	order = request.POST.order
+		#### will add 'order'(list box) in '8080.html' to sorting 
+		order = request.POST.order
 
-	if order : 
-		fstring = str1 + input_str + str2 + str3 + order
+		if order : 
+			fstring = str1 + input_str + str2 + str3 + order
+		else : 
+			fstring = str1 + input_str + str2
+
+		c.execute(fstring)
+		result=c.fetchall()
+		result = column + result
+		c.close()
+		output = template('make_table', rows=result)
+
+		return str(output)
 	else : 
-		fstring = str1 + input_str + str2
-
-	c.execute(fstring)
-	result=c.fetchall()
-	result = column + result
-	c.close()
-	output = template('make_table', rows=result)
-
-	return str(output)
-
+		notice = """
+			<h2>You should select at least one column</h2>
+			<form action = "http://52.78.114.3:8080/">
+				<input type="submit" value="back">
+			</form>
+			"""
+		return notice 
+	
 run(reloader = True, host='0.0.0.0', port=8080)
 
